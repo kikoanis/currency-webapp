364ea6dcdf4f1ba7ba165a6706286c0b94e3044f
Ali Hmer <ahmer@kruxanalytics.com>
2021-04-15 12:19:52 -0600

    2nd commit

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace currency_webapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
